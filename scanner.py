35d68c9d01706b2531ec64ac1e6e1bae29b51777
barasa <101450156+barasapeter@users.noreply.github.com>
2025-11-21 09:56:47 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -2,7 +2,7 @@ import urllib.request
 from urllib.parse import urlparse
 import os
 import shutil
-import pandas as pd
+import pandas as p
 
 def is_direct_download_link_question(text):
     pattern = r'(?i)\bhttps?://\S+\b'
